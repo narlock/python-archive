4b41f444981fe0e929b6d1320a71379625bb7fa2
Anthony <anthonynarlock@outlook.com>
2021-10-31 07:08:54 -0500

    Update primeNumbers.py

--- Review/Function Examples/primeNumbers.py
+++ Review/Function Examples/primeNumbers.py
@@ -21,10 +21,44 @@ def putPrimeNumbersIntoList(input):
 def sumSquaredList(list):
     sum = 0
     for i in range(len(list)):
-        sum = sum + (i * i)
+        sum = sum + (list[i] * list[i])
     return sum
 
+def jamMan():
+    loopCondition = True
+
+    while(loopCondition):
+        lower = 1
+        #1 Asks user to input a number N
+        upper = int(input("hello good sir, can you do me a favour and enter any random number here?: "))
+        inputUpper = upper
+        #2 All prime numbers 1 through N are printed
+        for number in range(lower,upper + 1):
+            if number >= 1:
+                for i in range(2,number):
+                    if (number % i) == 0:
+                        break
+                else:
+                    print(number)
+        #3 The sum of the square of all prime numbers 1 through N is computed and printed
+        squared_sum = 0
+        for number in range(lower,inputUpper + 1):
+            if number >= 1:
+                for i in range(2,number):
+                    if (number % i) == 0:
+                        break
+                else:
+                    squared_sum = squared_sum + (number * number)
+        print(squared_sum)
+
+        #4 The user is asked if they want to compute a new sum of squared numbers, if so, continue
+        continueCondition = input("Want to compute a new sum of squared? ")
+        if(continueCondition == "no"):
+            loopCondition = False
+    
+
 def main():
+    """
     myList = putPrimeNumbersIntoList(10)
     print("myList= ")
     print(myList)
@@ -32,6 +66,9 @@ def main():
     squared_sum = sumSquaredList(myList)
     print("sumSquaredLists = ")
     print(squared_sum)
+    """
+
+    jamMan()
     
 
 if __name__ == "__main__":
